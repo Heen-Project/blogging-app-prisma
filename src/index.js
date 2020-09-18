bc924914f053978f59c0c68ac774227cd03bc8e3
Heen-Project <61735806+Heen-Project@users.noreply.github.com>
2020-09-18 23:52:08 +0800

    init commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '@babel/polyfill'
+import '@babel/polyfill/noConflict'
 import { ApolloServer, PubSub } from 'apollo-server'
 import { resolvers, fragmentReplacements } from './resolvers/index'
 import typeDefs from './schema/schema'
